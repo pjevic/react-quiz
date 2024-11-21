9ada8dbdb3a7c07da230bc74154b7a27f6ee52c8
Milan Pjevic <milanpjevic@icloud.com>
2024-11-21 19:29:16 +0100

    Drafting App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 /** @format */
 
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
